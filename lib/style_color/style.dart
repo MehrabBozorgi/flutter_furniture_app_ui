c69c3e174fd1b27a407872fc7dc707e18536610a
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-07-30 12:36:50 +0430

    furniture_app_ui

---
+++ lib/style_color/style.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+const backGroundColor = Color(0xFFF5F5F5);
+const color2 = Color(0xFF413C5A);
+const color3 = Color(0xFFF8543B);
+const color4 = Color(0xFF8F8E96);
+
+const textStyle1 = TextStyle(
+  color: color2,
+  fontSize: 26,
+  fontWeight: FontWeight.bold,
+);
+const textStyle2 = TextStyle(
+  color: color2,
+  fontSize: 20,
+  fontWeight: FontWeight.normal,
+);
+const textStyle3 = TextStyle(
+  color: color2,
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+);
+const textStyle4 = TextStyle(
+  color: color2,
+  fontSize: 18,
+  fontWeight: FontWeight.normal,
+);
+const textStyle5 = TextStyle(
+  color: Colors.white,
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
+final textStyle6 = TextStyle(
+  color: Colors.grey[500],
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
+const textStyle7 = TextStyle(
+  color: color2,
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+);
